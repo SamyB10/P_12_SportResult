3b4b4816b210b6ff6310e5b38b28019b129e1757
samy Bouss <samy.boussair@gmail.com>
2023-08-25 08:42:19 +0200

    Update project

---
+++ ResultSport/Service/ConfigApi.swift
@@ -0,0 +1,12 @@
+//
+//  configApi.swift
+//  ResultSport
+//
+//  Created by Samy Boussair on 12/08/2023.
+//
+
+import Foundation
+
+public struct Key {
+    static let apiKey = "fe44ed9c4e186aaa62b040dc834a14a3c15526215ab9ce85c4c42c4373578f19"
+}
